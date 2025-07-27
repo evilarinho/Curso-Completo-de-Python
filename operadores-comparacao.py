c4b66073a21f0d6e8cf87293726c1fa035852102
edilson@edilson-Inspiron-15-3520 <ed.vilarinho@gmail.com>
2025-07-27 02:01:26 -0300

    2025-07-26

--- operadores-comparacao.py
+++ operadores-comparacao.py
@@ -5,9 +5,14 @@ print('Digite um número:')
 n1 = int(input())
 n2 = int(input('Digita outro número:'))
 
-x = n1 = n2
-print('São iguais?', x, '/n')
-      
+x = n1 == n2
+print('São iguais?', x, '\n')
+    
+z = n1 > n2
+print(n1, 'é maior que ', n2, '?', z, '\n')
+
+y = n1 != n2
+print('São diferentes?' + str(y))
       
 
 
