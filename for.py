8f415d2c22f33a6e2c94b2cf16ccd8d6c38fb49d
edilson@edilson-Inspiron-15-3520 <ed.vilarinho@gmail.com>
2025-09-12 01:08:14 -0300

    2025-09-11

---
+++ for.py
@@ -0,0 +1,34 @@
+# lista = [2,6,9,4,0,12,3,7]
+
+# for item in lista:
+#     print(item)
+
+# palavra = 'Bóson'
+
+# for letra in palavra:
+#     print(letra)
+
+# for numero in range(1,11):
+#     print(numero)
+
+# inicia com zero
+# for numero in range(10):
+#     print(numero)
+
+# nome = input('Digite seu nome: ')
+# for x in range(10):
+#     print(f'{x+1} {nome}')
+ 
+# range(valor _inicial, valor_final, incremento)|
+
+# for x in range(2,20,2):
+#     print(x)
+
+# for x in range(20,1,-2):
+#     print(x)
+
+pedras = ('Rubi', 'Esmeralda', 'Quartzo', 'Safira', 'Diamante', 'Turmalina')
+for pedra in pedras:
+    if pedra == 'Quartzo' :
+        continue
+    print (pedra)
---
+++ print.py
@@ -0,0 +1,47 @@
+# Sintaxe
+# print(obbjetos, argumentos)
+
+# mensagem = 'Função print()'
+# print(mensagem)
+# print('Aula de Python')
+
+# nome = 'Fábio dos Reis'
+# canal = 'Bóson Treinamentos'
+# print(canal, '-', nome)
+
+# nome = input('Digite seu nome: ')
+# print('Olá ' + nome + '! Bem-vindo ao curso de Python')
+
+# nome = input('Digite seu nome: ')
+# msg = 'Olá ' + nome + '! Bem-vindo ao curso de Python'
+# print(msg)
+
+# print('Imprime a mensagem e muda de linha')
+# print('Imprime a mensagem e permanece na linha.', end='')
+# print(' Continua na mesma linha!')
+
+# nome = 'Maria'
+# idade = 30
+
+# msg_formatada = 'O nome dela é {0} e ela tem {1} anos'.format(nome, idade)
+# print(msg_formatada)
+
+# print('O nome dela é {0} e ela tem {1} anos'.format(nome, idade))
+
+# nome = 'Fábio'
+# peso = 73.5
+
+# print(f'Ola, meu nome é {nome} e eu peso {peso} quilos.')
+
+# a= 10 
+# b = 5
+# res = f'A soma de {a} com {b} é igual a {a+b}'
+# print(res)
+
+# valor = 123.573637
+# print(f'O valor é {valor:.2f}')
+# print(f'O valor é \{valor:.2f}\'')
+
+nome = 'João'
+idade = 32
+print(f'Nome: {nome}\tIdade: {idade}')
