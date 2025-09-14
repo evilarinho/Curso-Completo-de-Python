7c2f522ab11923c9930910d12afc47e65b89ec7a
edilson@edilson-Inspiron-15-3520 <ed.vilarinho@gmail.com>
2025-09-14 02:17:35 -0300

    2025-09-13

--- modulos.py
+++ modulos.py
@@ -1,7 +1,25 @@
 #import math
 #from math import sqrt, sin
-import math as m
+#import math 
+#import mod_func as mf
+import numpy as np
 # from math import * | NÃO RECOMENDADO
 #print(math.sqrt(81))
 #print(sqrt(81))
-print(m.sqrt(144))
+#print(m.sqrt(144))
+
+# if __name__ == '__main__':
+#     mf.mensagem()
+
+#     num = int(input('Digite um número inteiro positivo: '))
+#     print(f'\nCalcular fatorial do número:')
+#     fat = mf.fatorial(num)
+#     print(f'O fatoral é {fat}')
+
+#     print(f'\nCalcular a sequência de fibonacci:')
+#     fib = mf.fibo(num)
+#     print(f'O fatoral é {fib}')
+
+L = np.array([1,2,3,4,5,6,7,8,9])
+print(L)
+
