2bd7b521957edf4754417c50a16885db1a3cc725
edilson@edilson-Inspiron-15-3520 <ed.vilarinho@gmail.com>
2025-09-28 23:23:08 -0300

    2025-09-28

---
+++ funcoes.py
@@ -0,0 +1,61 @@
+import os
+os.system('clear')
+
+# Funções
+# Modularização, Reúso de Codigo, Legibilidade
+
+# def mensagem():
+#     print('Bóson Treinamentos em Teconologia')
+#     print('Curso Completo de Python')
+
+# mensagem()
+
+# Função com argumentos
+# def soma(a,b):
+#     print(a+b)
+
+# soma(12,7)
+
+# def mult(x, y):
+#     return x * y
+
+
+# a = 5
+# b = 8
+# c = mult(a, b)
+# print(f'O produto de {a} e {b} é {c}')
+
+# def div(k, j):
+#     if j != 0:
+#         return k / j
+#     else:
+#         return 'Impossivel dividir por zero'
+
+# if __name__ == '__main__':
+#     a = int(input('Digite um número: '))
+#     b = int(input('Digite outro número: '))
+
+#     r = div(a, b)
+#     print(f'{a} dividido por {b} é igual a {r}')
+
+def quadrado(val):
+    quadrados = []
+    for x in val:
+        quadrados.append(x ** 2)
+    return quadrados
+
+if __name__ == '__main__':
+    valores = [2,5,7,9,12]
+    resultados = quadrado(valores)
+    for g in resultados:
+        print(g)
+
+
+
+    
+
+
+
+
+
+
