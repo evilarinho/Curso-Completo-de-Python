3c5d2a1b2bfd94ce27bd5a58f82fa71ec4c1fa59
edilson@edilson-Inspiron-15-3520 <ed.vilarinho@gmail.com>
2025-10-01 23:36:41 -0300

    2025-10-01

---
+++ escopo.py
@@ -0,0 +1,20 @@
+import os
+os.system('clear')
+
+# Escopo Global e Local
+
+var_global = 'Curso Completo de Python'
+def escreve_texto():
+    var_local = 'Fábio dos Reis'
+    print(f'Variável Global: {var_global}')
+    print(f'Variável Local: {var_local}')
+
+if __name__ == '__main__':
+    print('Executar a função escreve_texto()')
+    escreve_texto()
+
+    print('Tentar acessar as variáveis diretamente')
+    print(f'Variável Global: {var_global}')
+    #print(f'Variável Local: {var_local}')
+    #NameError: name 'var_local' is not defined. Did you mean: 'var_global'?
+
